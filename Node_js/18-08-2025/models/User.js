b12696cc719dbe6e6130eb07f9eb09f73db75cdc
Hemant Gupta <145823443+HemantG-17@users.noreply.github.com>
2025-08-19 13:38:46 +0530

    User.js

--- Node_js/18-08-2025/models/User.js
+++ Node_js/18-08-2025/models/User.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 const timeStamps=required('mongoose-timestamps')
 const Schema=mongoose.Schema;
 const userSchema=new Schema({
-    fisrtName:{type:String,required:true},
+    firstName:{type:String,required:true},
     lastName:{type:String,required:false},
     email:{type:String,required:true},
     password:{type:String,required:true},
