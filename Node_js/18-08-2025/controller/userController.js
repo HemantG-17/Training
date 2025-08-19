c89590c618449313b2587d32d0e9718621b289f9
Hemant Gupta <145823443+HemantG-17@users.noreply.github.com>
2025-08-19 13:38:07 +0530

    Update userController.js

--- Node_js/18-08-2025/controller/userController.js
+++ Node_js/18-08-2025/controller/userController.js
@@ -1,13 +1,44 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 async function addUser(req, res) {
     try {
         console.log(req.body, 'req.body');
-        let user=new user(req.body);
+        let user = new User(req.body);
+        user.usertype = 'Admin';
+        let encryptedPassword = bcrypt.hashSync(req.body.password, 10)
+        console.log(encryptedPassword, 'encryptedPassword')
+        user.password = encryptedPassword;
         await user.save();
-
+        res.redirect('/');
+        console.log("data saved successfully...")
     } catch (err) {
         console.log(err)
     }
 }
+
+async function doLogin(req,res){
+    try{
+      console.log(req.body,'req.body')
+      let user = await User.findOne({email:req.body.email})
+      console.log(user)
+      if(user){
+           let validPassword=bcrypt.compare(req.body.password,user.password)
+           if(validPassword){
+           res.render('welcomeAdmin')
+           }else{
+            res.end("invalid login/Password...")
+           }
+      }else{
+        console.log("no user found, please try login instead")
+      }
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+
 module.exports = {
     addUser,
+    doLogin
 }
