424bf339e61aeae0c8103b177d7fbea62a30d2ab
Hemant Gupta <hemantgupta132@gmail.com>
2025-08-28 11:03:55 +0530

    latest updated

--- Node_js/18-08-2025/controller/studentcontroller.js
+++ Node_js/18-08-2025/controller/studentcontroller.js
@@ -1,11 +1,27 @@
 const Student = require('../models/Student')
 const cloudinary = require('cloudinary').v2
+let result;
 async function addStudent(req, res) {
     try {
         console.log(req.body, 'req.body');
-        console.log(req.file,'req.file');
-        // let student = new Student(req.body);
-        // await student.save();
+        console.log(req.file, 'req.file');
+        if (req.file) {
+            cloudinary.config({
+                cloud_name: 'dufvmaweq',
+                api_key: '515135587521761',
+                api_secret: 'hlXM3UayaeIK5SYHFnPZpZIV1wA',
+            });
+            result = await cloudinary.uploader.upload(req.file.path);
+            console.log(result);
+
+        }
+        let student = new Student(req.body);
+        if (req.file) {
+            student.studentImage = result.secure_url;
+        }
+
+        await student.save();
+        console.log("database updated.......")
         let students = await Student.find({})
         res.render('studentList', {
             students: students
@@ -58,17 +74,17 @@ async function saveEditedStudent(req, res) {
         let studentId = req.params._id
         console.log(studentId, 'studentId');
         let student = await Student.findOne({ _id: studentId })
-        student.rollNo=req.body.rollNo;
-        student.name=req.body.name;
-        student.fatherName=req.body.fatherName;
-        student.course=req.body.course;
-        student.branch=req.body.branch;
-        student.yearOfAdm=req.body.yearOfAdm;
+        student.rollNo = req.body.rollNo;
+        student.name = req.body.name;
+        student.fatherName = req.body.fatherName;
+        student.course = req.body.course;
+        student.branch = req.body.branch;
+        student.yearOfAdm = req.body.yearOfAdm;
         await student.save();
         console.log("student Updated Successfully......")
         let students = await Student.find({})
-        res.render('welcomeAdmin',{
-            students:students
+        res.render('welcomeAdmin', {
+            students: students
         });
 
     } catch (err) {
